54aef02ed302f1c2348f8c035fb36d0c2a5ba43e
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-11-02 19:03:23 -0400

    Navbar and home page created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// Edited App.js by Jack Loveday
 
+// Import dependencies
+import './App.css';
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/pages/Home";
+//import { About } from "./components/Pages/About";
+//import { Blog } from "./components/Pages/Blog";
+//import { Contact } from "./components/Pages/Contact";
+
+// App functionality, react-dom
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+// <Route path="/about" component={About} />
+// <Route path="/blog" component={Blog} />
+// <Route path="/contact" component={Contact} />
